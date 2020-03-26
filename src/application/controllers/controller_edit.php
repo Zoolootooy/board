e94f8a8ba4188becbe3c31a699353832da080ee6
Maksym Haidash <maxradjia.zp@gmail.com>
2020-03-26 13:34:50 +0200

    Updated edit controller

--- src/application/controllers/controller_edit.php
+++ src/application/controllers/controller_edit.php
@@ -1,1 +1,13 @@
 <?php
+require '../models/advert.php';
+
+
+//запросить модель
+$filename = uploadImage($_FILES['photo']);
+addAdvert($_POST['title'], $_POST['description'], $_POST['phone'], $_POST['country_id'], $_POST['email'],
+    $_POST['end_date'],  $filename);
+//echo '<pre>';
+//var_dump($_POST['title'], $_POST['description'], $_POST['phone'], $_POST['country_id'], $_POST['email'],
+//    $_POST['end_date'],  $_FILES['photo']);
+//echo '</pre>';
+header("Location: /");
