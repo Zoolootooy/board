2d45c11578307c7098d166794f27ef5697279028
Maksym Haidash <maxradjia.zp@gmail.com>
2020-03-26 16:25:56 +0200

    Updated edit view. Added the map

--- src/application/views/edit_view.php
+++ src/application/views/edit_view.php
@@ -5,7 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit</title>
+    <title>Create new</title>
+    <style>
+        #map{
+            height: 400px;
+            width: 500px;
+        }
+    </style>
 </head>
 <body>
 
@@ -38,6 +44,42 @@
             <button class="createForm__Submit" type="submit">Save</button>
         </form>
     </div>
+
+    <div id="map"></div>
+
+    <script>
+        var marker;
+        var markerCounter = 0;
+        function initMap() {
+            var myPos = {lat: 47.867045, lng: 35.166591};
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: myPos,
+                zoom: 8
+            });
+            
+
+            google.maps.event.addListener(map, 'click', function(event) {
+                placeMarker(event.latLng);
+            });
+        }
+
+        function placeMarker(location) {
+            if ( marker ) {
+                marker.setPosition(location);
+            } else {
+                marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                });
+            }
+        }
+
+
+
+    </script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAk10RdU1wJJ7UZnIZj8XBuVQopBvicRPE&callback=initMap"
+            async defer></script>
 
 </body>
 </html>
