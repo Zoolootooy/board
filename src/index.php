ff5c41f5f31b7af32127dbef294ee143398ef087
Maksym Haidash <maxradjia.zp@gmail.com>
2020-03-30 14:38:46 +0200

    Updated index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,10 @@
 <?php
+use application\core\Router;
+use application\core\Request;
+
+ini_set('display_errors', 0);
+require __DIR__.'/vendor/autoload.php';
 
 
-include "application/views/list_view.php";
+Router::load('application/routes.php')
+    ->direct(Request::uri(), Request::method());
